b561da29de8b6602db5d70f2429246c709144921
Myungchul.Lee <mgcllee@gmail.com>
2022-09-20 02:02:31 +0900

    init
    init

---
+++ Source/PokeHunter/PokeHunter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PokeHunter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PokeHunter, "PokeHunter" );
---
+++ Source/PokeHunter/PokeHunterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PokeHunterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POKEHUNTER_API APokeHunterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

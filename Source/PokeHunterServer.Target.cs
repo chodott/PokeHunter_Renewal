bccd7ed86f993a94758ffaf8733a71c537a13c69
Mgcllee <mgcllee@gmail.com>
2022-09-20 20:18:06 +0900

    [Server] DedicatedServer Settings (#01)
    UE4.27 DedicatedServer first settings

---
+++ Source/PokeHunterServer.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+using System;
+
+public class PokeHunterServerTarget : TargetRules //이 행은 프로젝트 이름에 맞게 변경합니다.
+{
+    public PokeHunterServerTarget(TargetInfo Target) : base(Target) //이 행은 프로젝트 이름에 맞게 변경합니다.
+    {
+        Type = TargetType.Server;
+        DefaultBuildSettings = BuildSettingsVersion.V2;
+        ExtraModuleNames.Add("PokeHunter"); //이 행은 프로젝트 이름에 맞게 변경합니다.
+    }
+}

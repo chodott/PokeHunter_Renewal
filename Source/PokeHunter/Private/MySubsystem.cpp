ac2af29cbab07ee4723588c28d6c05b7688b9400
mgcllee <mgcllee@gmail.com>
2023-03-13 02:47:32 +0900

    Client Network Module 설계완료
    소켓연결 준비 끝

--- Source/PokeHunter/Private/MySubsystem.cpp
+++ Source/PokeHunter/Private/MySubsystem.cpp
@@ -4,22 +4,23 @@
 
 UMySubsystem::UMySubsystem() 
 {
-
+	Socket = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateSocket(NAME_Stream, TEXT("default"), false);
+	// TSharedRef<FInternetAddr> addr = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateInternetAddr();
 }
 
 void UMySubsystem::ConnectToServer()
 {
-	FSocket* Socket = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateSocket(NAME_Stream, TEXT("default"), false);
+	address = TEXT("127.0.0.1");
+	port = 7777;
 
-	FString address = TEXT("127.0.0.1");
-	int32 port = 7777;
-	FIPv4Address ip;
 	FIPv4Address::Parse(address, ip);
 
 	TSharedRef<FInternetAddr> addr = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateInternetAddr();
 	addr->SetIp(ip.Value);
-	addr->SetPort(port);
-
-	/*GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Trying to connect.")));
-	bool conneted = Socket->Connect(*addr);*/
+	addr->SetPlatformPort(port);
+	// addr->SetPort(7777);
+	
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Trying to connect.")));
+	
+	bool conneted = Socket->Connect(*addr);
 }
--- Source/PokeHunter/Public/MySubsystem.h
+++ Source/PokeHunter/Public/MySubsystem.h
@@ -1,12 +1,13 @@
 #pragma once
 
+#include "Networking.h"
+#include "Sockets.h"
+#include "SocketSubsystem.h"
+
 #include "CoreMinimal.h"
 #include "Subsystems/Subsystem.h"
 
 // #include "Engine.h"
-#include "Networking.h"
-#include "SocketSubsystem.h"
-#include "Sockets.h"
 
 #include "MySubsystem.generated.h"
 
@@ -20,6 +21,12 @@ class POKEHUNTER_API UMySubsystem : public USubsystem
 		UMySubsystem();
 	
 public:
+	FSocket* Socket;
+
+	FString address;
+	int32 port;
+	FIPv4Address ip;
+
 	UFUNCTION(Blueprintcallable, Category=Socket)
 		void ConnectToServer();
 

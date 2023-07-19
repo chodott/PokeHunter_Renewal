92d456820b541f9736ba3488f2f62cc8069ce43b
chodot <89974193+chodott@users.noreply.github.com>
2023-07-20 03:25:45 +0900

    Test Bullet Collsiion

--- Source/PokeHunter/Item/Bullet.h
+++ Source/PokeHunter/Item/Bullet.h
@@ -37,10 +37,12 @@ public:
 
 	virtual void BeginPlay() override;
 
-	UFUNCTION()
 	//void OnHit(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
-	
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 	virtual void UseItem(APawn* ItemOwner, FVector InitialPos, FVector EndPos);
 
 	UFUNCTION(NetMulticast, Reliable)
